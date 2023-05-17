8e29c246d0c65e18b2d450f6080287d9e68edab3
Burak İbaoğlu <buca_42@hotmail.com>
2023-05-17 06:41:57 +0300

    Vite favicon generator plugin added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+
 
 export default defineConfig({
   server: {
     port: 8080
-  }
+  },
+  plugins: [
+    vitePluginFaviconsInject('./public/images/favicon.svg')
+  ]
 })
